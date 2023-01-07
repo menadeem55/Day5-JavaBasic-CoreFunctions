59b9096e6d959f5b150062035a9f324bbecf4c03
me.nadeem55@gmail.com <me.nadeem55@gmail.com>
2023-01-08 00:30:41 +0530

    Added: To check Euclidean distance value

--- src/com/bridgelabz/basicjava/Distance.java
+++ src/com/bridgelabz/basicjava/Distance.java
@@ -5,18 +5,15 @@ import java.util.Scanner;
 public class Distance {
 
 	public static void main(String[] args) {
-		int x;
-		int y;
-		double distance =0.0;
-		Scanner scanner = new Scanner(System.in);	//user input
-		System.out.println("Enter the value of x: ");
-		x = scanner.nextInt();	//save user input variable
-		
-		System.out.println("Enter the value of y: ");
-		y= scanner.nextInt();	//save user input variable
-			distance = ((double)Math.pow((x * x + y * y),0.5));	//inbuilt method
-			
-				System.out.println("distance is :"+distance);
+		Scanner scanner = new Scanner(System.in); // scanner input
+		System.out.println("Enter the value of x");
+		int x = scanner.nextInt();
+		System.out.println("Enter the value of y");
+		int y = scanner.nextInt();
+
+		double distance = Math.sqrt(x * x + y * y); // formula for distance
+		System.out.println("Euclidean distance : " + distance);
+
 	}
 
 }
